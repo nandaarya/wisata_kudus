86970571bbf33aa50bfac68f800b92c292e3ee73
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-06 21:58:18 +0700

    (+) Menambahkan 2 fonts (Oxygen-Regular.ttf & Staatliches-Regular.ttf) (+) Mengubah fonts pada beberapa bagian (+) Menambahkan border radius pada images (-) Menghapus widget SafeArea

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,89 +1,110 @@
 import 'package:flutter/material.dart';
 
+var informationTextStyle = TextStyle(fontFamily: 'Oxygen');
+
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Image.asset('images/menara_kudus.jpg'),
-              Container(
-                  margin: EdgeInsets.only(top: 16.0),
-                  child: Text(
-                      'Menara Kudus',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                      )
-                  )
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.calendar_today),
-                        SizedBox(height: 8.0),
-                        Text('Buka Setiap Hari'),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.query_builder),
-                        SizedBox(height: 8.0),
-                        Text('00.00 - 24.00'),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.monetization_on),
-                        SizedBox(height: 8.0),
-                        Text('Gratis'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset('images/menara_kudus.jpg'),
+            Container(
+                margin: EdgeInsets.only(top: 16.0),
                 child: Text(
-                  'Sebagai salah satu tempat bersejarah di Kudus, Menara kudus menjadi objek wisata religi yang tidak pernah sepi pengunjung. Di kawasan Menara Kudus terdapat makan Sunan Kudus yang selalu dikunjungi pengunjung untuk berziarah. selain itu, Menara dan Masjid dikawasan depan Menara Kudus juga menjadi spot foto yang bagus bagi pengunjung.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16.0),
-                ),
+                    'Menara Kudus',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Staatliches',
+                      fontSize: 30.0,
+                    )
+                )
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Buka Setiap Hari',
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.query_builder),
+                      SizedBox(height: 8.0),
+                      Text(
+                        '00.00 - 24.00',
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text(
+                        'Gratis',
+                        style: informationTextStyle,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
+            ),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Sebagai salah satu tempat bersejarah di Kudus, Menara kudus menjadi objek wisata religi yang tidak pernah sepi pengunjung. Di kawasan Menara Kudus terdapat makan Sunan Kudus yang selalu dikunjungi pengunjung untuk berziarah. selain itu, Menara dan Masjid dikawasan depan Menara Kudus juga menjadi spot foto yang bagus bagi pengunjung.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
                       child: Image.asset('images/menara_kudus_1.jpg'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
                       child: Image.asset('images/menara_kudus_2.jpg'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
                       child: Image.asset('images/menara_kudus_3.jpg'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
                       child: Image.asset('images/menara_kudus_4.jpg'),
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
