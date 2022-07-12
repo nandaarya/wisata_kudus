b3ded7a10c8669b8273bf5b6953ad8cb66ba0ebc
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-12 11:38:02 +0700

    (+) add visual TourismPlaceGrid widget

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -79,7 +79,45 @@ class TourismPlaceGrid extends StatelessWidget {
       padding: const EdgeInsets.all(24.0),
       child: GridView.count(
         crossAxisCount: 4,
-        children: [],
+        children: tourismPlaceList.map((place) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place: place);
+              }));
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      place.imageAsset,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      place.name,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(
+                      place.location,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
