3491b8732749ca98255b2b78572c6ce9f0b7ce72
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-12 11:22:30 +0700

    (+) add TourismPlaceList widget

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,50 +9,57 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Wisata Kudus. Size: ${MediaQuery.of(context).size.width}'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final TourismPlace place = tourismPlaceList[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen(place: place);
-              }));
-            },
-            child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Image.asset(place.imageAsset),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            place.name,
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(place.location),
-                        ],
-                      ),
+      body: TourismPlaceList(),
+    );
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailScreen(place: place);
+            }));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(place.imageAsset),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          place.name,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(place.location),
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-          );
-        },
-        itemCount: tourismPlaceList.length,
-      ),
+          ),
+        );
+      },
+      itemCount: tourismPlaceList.length,
     );
   }
 }
