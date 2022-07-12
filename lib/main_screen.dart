60b7d6b00ed758bc6773c3bc02e00df81f36e3bd
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-12 11:56:32 +0700

    (+) add space between items

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -85,6 +85,8 @@ class TourismPlaceGrid extends StatelessWidget {
       padding: const EdgeInsets.all(24.0),
       child: GridView.count(
         crossAxisCount: gridCount,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
         children: tourismPlaceList.map((place) {
           return InkWell(
             onTap: () {
