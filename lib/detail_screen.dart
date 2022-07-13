7cd3f25ae4e6cc78b7365505f8d186228544af6e
Nanda Arya Putra <nandaarya404@gmail.com>
2022-07-13 22:16:15 +0700

    (+) add ScrollController

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -143,9 +143,22 @@ class DetailMobilePage extends StatelessWidget {
   }
 }
 
-class DetailWebPage extends StatelessWidget {
+class DetailWebPage extends StatefulWidget {
   final TourismPlace place;
+
   const DetailWebPage({required this.place});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -176,18 +189,20 @@ class DetailWebPage extends StatelessWidget {
                       child: Column(
                         children: [
                           ClipRRect(
-                            child: Image.asset(place.imageAsset),
+                            child: Image.asset(widget.place.imageAsset),
                             borderRadius: BorderRadius.circular(10),
                           ),
                           SizedBox(height: 16),
                           Scrollbar(
                             thumbVisibility: true,
+                            controller: _scrollController,
                             child: Container(
                               height: 150,
                               padding: const EdgeInsets.only(bottom: 16),
                               child: ListView(
+                                controller: _scrollController,
                                 scrollDirection: Axis.horizontal,
-                                children: place.imageAsset2.map((images) {
+                                children: widget.place.imageAsset2.map((images) {
                                   return Padding(
                                     padding: const EdgeInsets.all(4.0),
                                     child: ClipRRect(
@@ -212,7 +227,7 @@ class DetailWebPage extends StatelessWidget {
                             children: <Widget>[
                               Container(
                                 child: Text(
-                                  place.name,
+                                  widget.place.name,
                                   textAlign: TextAlign.center,
                                   style: TextStyle(
                                     fontSize: 30.0,
@@ -228,7 +243,7 @@ class DetailWebPage extends StatelessWidget {
                                       Icon(Icons.calendar_today),
                                       SizedBox(width: 8.0),
                                       Text(
-                                        place.openDays,
+                                        widget.place.openDays,
                                         style: informationTextStyle,
                                       ),
                                     ],
@@ -241,7 +256,7 @@ class DetailWebPage extends StatelessWidget {
                                   Icon(Icons.access_time),
                                   SizedBox(width: 8.0),
                                   Text(
-                                    place.openTime,
+                                    widget.place.openTime,
                                     style: informationTextStyle,
                                   ),
                                 ],
@@ -252,7 +267,7 @@ class DetailWebPage extends StatelessWidget {
                                   Icon(Icons.monetization_on),
                                   SizedBox(width: 8.0),
                                   Text(
-                                    place.ticketPrice,
+                                    widget.place.ticketPrice,
                                     style: informationTextStyle,
                                   ),
                                 ],
@@ -260,7 +275,7 @@ class DetailWebPage extends StatelessWidget {
                               Container(
                                 padding: EdgeInsets.symmetric(vertical: 16.0),
                                 child: Text(
-                                  place.description,
+                                  widget.place.description,
                                   textAlign: TextAlign.justify,
                                   style: TextStyle(
                                     fontSize: 16.0,
